8ba26147f626f5d09cfd639e8fa6ff2438188129
trinity3ru <trinity3ru@gmail.com>
2025-10-16 10:58:20 +0400

    Update user.py
    добавили id Юзеру

--- models/user.py
+++ models/user.py
@@ -1,12 +1,21 @@
 
 
 class User:
-    def  __init__(self, username, password, email):
+    def  __init__(self, login: str, password:str, email: str, id: int|None = None, username: str|None = None):
         self.username = username
         self.password = password
         self.email = email
+        self.login = login
+        self.id = id
+
+    def get_id(self)-> int|None:
+        return self.id
 
     def greet(self):
-        return "Привет я пользователь, мой ник {self.username}"    
+        if self.username:
+            return f"Привет я пользователь, мой ник {self.username}"   
+        else:
+            return f"Привет я пользователь, мой ник {self.login}"    
+        
     def ret(self):
-        return  f"{self.username} | {self.password}  | {self.email}"
+        return  f"{self.username or self.login}| {self.password}  | {self.email}|{self.id}"
