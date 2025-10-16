ae1fb6d653855a716c8b3f171ff98d224476c338
trinity3ru <trinity3ru@gmail.com>
2025-10-16 11:21:16 +0400

    Update bot.py
    update bot

--- models/bot.py
+++ models/bot.py
@@ -1,20 +1,25 @@
-from .user import User
+ 
 
 class Bot:
-    def __init__(self, owner:int, token:str, git_http:str):
-        self._owner = owner
-        self._token = token
-        self._gitHttp = git_http
+    def __init__(self, user_id:int, token:str, git_http:str):
+        self.user_id = user_id
+        self.token = token
+        self.git_http = git_http
 
 
 
-    def get_ownerID(self) ->int:
-        return self._owner.get_id()
+    def get_owner_id(self) ->int:
+        return self.user_id
 
     def get_token(self)->str:
-        return self._token
+        return self.token
 
 
     def get_git(self)->str:
-        return self._gitHttp
+        return self.git_http
+    
+
+    def __repr__(self):
+        return  f" <Вот user_id {self.user_id}, token=' {self.token[:5]}...', git=' {self.git_http}'>"
+
 
